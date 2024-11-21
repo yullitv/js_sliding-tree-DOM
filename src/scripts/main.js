43bb797d43d5251fc28215e08799bc0bba9c7655
Yullit <yuliok03@gmail.com>
2024-11-21 13:05:03 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+function sliding(e) {
+  const targetLi = e.target.closest('li');
+  const subList = targetLi.querySelector('ul');
+
+  if (targetLi && subList) {
+    subList.style.display = subList.style.display === 'none' ? '' : 'none';
+  }
+}
+
+tree.addEventListener('click', sliding);
