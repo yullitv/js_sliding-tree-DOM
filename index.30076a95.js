b701fd9e59371309b11ec98fbd9e91b3d773e402
Yullit <yuliok03@gmail.com>
2024-11-21 13:05:35 +0200

    make build

---
+++ index.30076a95.js
@@ -0,0 +1,2 @@
+document.querySelector(".tree").addEventListener("click",function(e){var t=e.target.closest("li"),l=t.querySelector("ul");t&&l&&(l.style.display="none"===l.style.display?"":"none")});
+//# sourceMappingURL=index.30076a95.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const tree = document.querySelector(".tree");
+function sliding(e) {
+    const targetLi = e.target.closest("li");
+    const subList = targetLi.querySelector("ul");
+    if (targetLi && subList) subList.style.display = subList.style.display === "none" ? "" : "none";
+}
+tree.addEventListener("click", sliding);
+
+//# sourceMappingURL=index.f75de5e1.js.map
